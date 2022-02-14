42d7b147c770a35ef6226026917f02217e7786f1
oneneoncoffee <76235757+oneneoncoffee@users.noreply.github.com>
2022-02-13 19:52:28 -0700

    M5timex.ino Watch version
     This is an example of A working NTP client with A pool server; I use and trust for the most part. Smart watches are awesome tech that relies mainly on NTP cluster computers for time stamps. This sketch was made for the M5StickC plus and/or the M5 Stack Stick C internet of things device.    I have included URLS and A description of how the math works to get GMT/UTC time offsets. This sketch also updates the Real Time Clock on the M5 device and has A offline mode as well I have included A on the go mode for the device.

---
+++ M5WatchWhiteCat/include_def.h
@@ -0,0 +1,29 @@
+const char* ssid = "myessidxxx";
+const char* password = "mypasswordxxx"; 
+
+//Color defined codes 256 pallet 
+#define yellow 0xff80
+#define red 0xe8e4
+#define pink 0xfd79
+#define orange 0xfbe4 
+#define green 0x2589
+#define blue 0x52d
+#define darkblue 0x3a56
+#define violet 0xa254
+#define gray 0x7bef
+#define black 0x00
+
+// values for x bitmap files. 
+#define IW 320 
+#define IH 40
+#define XX 5700
+#define AX 230
+#define IW2 50
+#define IH2 50
+
+// Defined buttons on M5Stick 
+#define A 37
+#define B 39
+#define LED 10
+// Delay defined 
+#define WAIT 355 
